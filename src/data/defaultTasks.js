3f4e085063193a61737e279e1070ab404e5ca9ef
gemaquejr <gemaquejr@hotmail.com>
2024-09-22 09:54:59 -0300

    Adiciona arquivo de dados padrão
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

---
+++ src/data/defaultTasks.js
@@ -0,0 +1,8 @@
+const defaultTasks = [
+    { id: 1, text: 'Lavar as mãos', completed: false },
+    { id: 2, text: 'Fazer um bolo', completed: false },
+    { id: 3, text: 'Lavar a louça', completed: false },
+    { id: 4, text: 'Levar o lixo para fora', completed: true }
+  ];
+  
+  export default defaultTasks;
