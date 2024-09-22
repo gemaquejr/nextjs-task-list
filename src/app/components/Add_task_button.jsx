50f19375c3d424a3104682f9dfec1573df584af1
gemaquejr <gemaquejr@hotmail.com>
2024-09-22 20:00:07 -0300

    Adiciona componente Add_task_button.jsx
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

---
+++ src/app/components/Add_task_button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './../styles/addTaskButton.scss'
+
+const AddTaskButton = () => {
+  return (
+      <button className="add-task-button">
+        <span>Adicionar nova tarefa</span>
+      </button>
+  );
+};
+
+export default AddTaskButton;
--- src/app/components/home_tarefas_desktop.jsx
+++ src/app/components/home_tarefas_desktop.jsx
@@ -3,19 +3,22 @@
 import React from 'react';
 import Header from './Header';
 import Tasks from './Tasks';
+import AddTaskButton from './Add_task_button';
 
 import './../styles/globals.scss'
 
 const Home_tarefas_desktop = () => {
 
   return (
-    <div className="todo-list-container">
-      <Header />
+    <>
+      <div className="todo-list-container">
+        <Header />
 
-      <Tasks />
-
-      <button className="add-task-button">Adicionar nova tarefa</button>
-    </div>
+        <Tasks />
+        
+        <AddTaskButton />
+      </div>
+    </>
   );
 };
 
