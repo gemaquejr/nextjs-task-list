0d633720cd382f3c3642653b03a114fdf4f3230e
gemaquejr <gemaquejr@hotmail.com>
2024-09-22 15:18:09 -0300

    Adiciona componente Tasks.jsx
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

---
+++ src/app/components/Tasks.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import Image from 'next/image';
+import defaultTasks from './../../data/defaultTasks';
+import './../styles/tasks.scss'
+
+const Tasks = () => {
+    const [tasks, setTasks] = useState(defaultTasks);
+
+    return (
+        <div className="task-container">
+        <div className="task-section">
+            <h2>Suas tarefas de hoje</h2>
+            <ul>
+            {tasks.filter(task => !task.completed).map(task => (
+                <li key={task.id}>
+                <div className="task-item">
+                    <input
+                        type="checkbox"
+                        checked={task.completed}
+                        onChange={() => {
+                            setTasks(tasks.map(t => 
+                                t.id === task.id ? { ...t, completed: !t.completed } : t
+                            ));
+                        }}
+                    />
+                    <span className="task-text">{task.text}</span>
+                </div>
+                <button onClick={() => openDeleteModal(task.id)}>
+                    <Image src="/assets/Icon.png" alt="Lixeira" width={18} height={20} />
+                </button>
+                </li>
+            ))}
+            </ul>
+        </div>
+
+        <div className="completed-section">
+            <h2>Tarefas finalizadas</h2>
+            <ul>
+            {tasks.filter(task => task.completed).map(task => (
+                <li key={task.id} className="completed-task">
+                    <div className="task-item">
+                        <input
+                            type="checkbox"
+                            className="completed-checkbox"
+                            checked
+                            disabled
+                        />
+                        <span className="task-text">{task.text}</span>
+                    </div>
+                    <button onClick={() => openDeleteModal(task.id)}>
+                        <Image src="/assets/Icon.png" alt="Lixeira" width={18} height={20} />
+                    </button>
+                </li>
+            ))}
+            </ul>
+        </div>
+        </div>
+    );
+};
+
+export default Tasks;
--- src/app/components/home_tarefas_desktop.jsx
+++ src/app/components/home_tarefas_desktop.jsx
@@ -1,43 +1,18 @@
 'use client';
 
 import React from 'react';
-import Image from 'next/image';
 import Header from './Header';
+import Tasks from './Tasks';
+
+import './../styles/globals.scss'
 
 const Home_tarefas_desktop = () => {
+
   return (
     <div className="todo-list-container">
       <Header />
 
-      <div className="task-section">
-        <h2>Suas tarefas de hoje</h2>
-        <span>Lavar as mãos</span>
-        <input type="checkbox" />
-        <span>
-          <Image src="/assets/Icon.png" alt="Lixeira" width={18} height={20} />
-        </span>
-
-        <span>Fazer um bolo</span>
-        <input type="checkbox" />
-        <span>
-          <Image src="/assets/Icon.png" alt="Lixeira" width={18} height={20} />
-        </span>
-
-        <span>Lavar a louça</span>
-        <input type="checkbox" />
-        <span>
-          <Image src="/assets/Icon.png" alt="Lixeira" width={18} height={20} />
-        </span>
-      </div>
-
-      <div className="completed-section">
-        <h2>Tarefas finalizadas</h2>
-        <span>Levar o lixo para fora</span>
-        <input type="checkbox" />
-        <span>
-          <Image src="/assets/Icon.png" alt="Lixeira" width={18} height={20} />
-        </span>
-      </div>
+      <Tasks />
 
       <button className="add-task-button">Adicionar nova tarefa</button>
     </div>
